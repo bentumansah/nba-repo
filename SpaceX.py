0482129d564918ba7716d17ac30501b77092defb
Nana Bentum <162124181+bentumansah@users.noreply.github.com>
2024-03-09 21:32:49 +0000

    spacex

--- SpaceX.py
+++ SpaceX.py
@@ -3,3 +3,17 @@ middle_name = "Bentum "
 last_name = "Ansah"
 full_name = first_name + middle_name + last_name + "." 
 print(f"Hello, welcome {full_name}")
+
+
+#write a code that has 10 elements in a list
+#for which the code sums the elements in the lists using a for loop
+marks = [90, 88, 94, 95, 89, 97, 87, 93, 91, 96]
+
+sum = 0
+
+
+for mark in marks:
+    sum += mark
+    print(sum)
+
+ 
