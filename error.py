f28f73ed3a835abab361e0eb2a000e3dcc196281
Nana Bentum <162124181+bentumansah@users.noreply.github.com>
2024-03-09 18:22:03 +0000

    A new error occured

--- error.py
+++ error.py
@@ -8,3 +8,24 @@ def family():
 
 
 family()
+x = float(input("Enter the first number: "))
+
+y = float(input("Enter the second number: "))
+
+print("""1. Add
+2. Subtract
+3. Multiply
+4. Divide""")
+
+mathematical_operator = input("Enter the operator: ")
+
+if mathematical_operator == 1:
+    print(f"The sum of the operands is {x + y}")
+elif mathematical_operator == 2:
+    print(f"The difference between the operands is {x - y}")
+elif mathematical_operator == 3:
+    print(f"The product of the operands is {x * y}")
+elif mathematical_operator == 4:
+    print(f"The quotient of the operands is {x / y}")
+else:
+    print("Invalid operation")
