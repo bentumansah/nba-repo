86c604769897cc8d9b62d1d0bd399e80169d023c
Nana Bentum <162124181+bentumansah@users.noreply.github.com>
2024-03-09 21:21:39 +0000

    for loop list elements sum up

--- nba.py
+++ nba.py
@@ -2,3 +2,14 @@ x = 5
 y = 6
 sum = x + y
 print(sum)
+#write a code that has 10 elements in a list
+#for which the code sums the elements in the lists using a for loop
+marks = [90, 88, 94, 95, 89, 97, 87, 93, 91, 96]
+
+sum = 0
+
+
+for mark in marks:
+    sum += mark
+    print(sum)
+
