96f6986febd8e50a9db799c3229b68691dae2221
Nana Bentum <162124181+bentumansah@users.noreply.github.com>
2024-03-09 21:08:37 +0000

    Exercise

---
+++ h_exercise.py
@@ -0,0 +1,11 @@
+#write a code that has 10 elements in a list
+#for which the code sums the elements in the lists using a for loop
+marks = [90, 88, 94, 95, 89, 97, 87, 93, 91, 96]
+
+sum = 0
+
+
+for mark in marks:
+    sum += mark
+    print(sum)
+
